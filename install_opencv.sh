76d2e3706dfe54d3819c41e6ffbbeb8c26b3134c
SimsGautam <simantagautam@gmail.com>
2016-12-08 19:50:00 -0500

    removed opencv to separate script

---
+++ install_opencv.sh
@@ -0,0 +1,8 @@
+## for opencv
+sudo apt-get --yes --force-yes install build-essential
+sudo apt-get --yes --force-yes install libgtk2.0-dev
+sudo apt-get --yes --force-yes install pkg-config
+sudo apt-get --yes --force-yes install libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get --yes --force-yes install libtbb2 libtbb-dev libpng-dev libjasper-dev libdc1394-22-dev
+sudo apt-get --yes --force-yes install libopencv-dev python-opencv
+##
