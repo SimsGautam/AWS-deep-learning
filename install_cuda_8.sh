6a24b7f7d2901a2140ee3c93cf1a188c784df88f
SimsGautam <simantagautam@gmail.com>
2017-06-26 04:05:36 +0000

    update to cuda install

--- install_cuda_8.sh
+++ install_cuda_8.sh
@@ -9,7 +9,7 @@ sudo chown ubuntu:ubuntu /mnt/bin
 
 # install the required packages
 sudo apt-get update && sudo apt-get -y upgrade
-sudo apt-get -y install linux-headers-$(uname -r) linux-image-extra-`uname -r`
+#sudo apt-get -y install linux-headers-$(uname -r) linux-image-extra-`uname -r`
 
 # install cuda
 wget https://developer.nvidia.com/compute/cuda/8.0/prod/local_installers/cuda-repo-ubuntu1404-8-0-local_8.0.44-1_amd64-deb
