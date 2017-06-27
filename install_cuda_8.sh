c733abe05bbb716d2caf1d8b7a6348d3deb26444
SimsGautam <simantagautam@gmail.com>
2017-06-27 16:16:11 +0000

    updated cuda download for 16.04

--- install_cuda_8.sh
+++ install_cuda_8.sh
@@ -12,8 +12,8 @@ sudo apt-get update && sudo apt-get -y upgrade
 #sudo apt-get -y install linux-headers-$(uname -r) linux-image-extra-`uname -r`
 
 # install cuda
-wget https://developer.nvidia.com/compute/cuda/8.0/prod/local_installers/cuda-repo-ubuntu1404-8-0-local_8.0.44-1_amd64-deb
-sudo dpkg -i cuda-repo-ubuntu1404-8-0-local_8.0.44-1_amd64-deb
-rm cuda-repo-ubuntu1404-8-0-local_8.0.44-1_amd64-deb
+wget https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64-deb
+sudo dpkg -i cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64-deb
+rm cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64-deb
 sudo apt-get update
 sudo apt-get install -y cuda
