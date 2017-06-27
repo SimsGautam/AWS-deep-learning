dfa8460f76384817f46dd4266224550de3c65bd5
Ubuntu <SimsGautam>
2017-06-27 17:09:41 +0000

    updated tensorflow to pip

---
+++ install_monitoring_tools.sh
@@ -0,0 +1,5 @@
+#install monitoring programs
+sudo wget https://git.io/gpustat.py -O /usr/local/bin/gpustat
+sudo chmod +x /usr/local/bin/gpustat
+sudo nvidia-smi daemon
+sudo apt-get -y install htop
