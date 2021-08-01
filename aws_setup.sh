040354ad4b50e6fac01a08838781ab28326fae3d
Axarva <timsinaatharva@gmail.com>
2021-08-01 19:49:30 +0545

    Remove redundancy in aws_setup shell file

--- aws_setup.sh
+++ aws_setup.sh
@@ -1,26 +1,29 @@
-sudo apt-get --yes --force-yes update
-sudo apt-get --yes --force-yes install git
-sudo apt-get --yes --force-yes install cmake
-sudo apt-get --yes --force-yes install python-pip
-sudo apt-get --yes --force-yes install python-dev
-sudo apt-get --yes --force-yes install libboost-all-dev
-sudo apt-get --yes --force-yes install libtiff5-dev
-sudo apt-get --yes --force-yes install libjpeg8-dev
-sudo apt-get --yes --force-yes install zlib1g-dev
-sudo apt-get --yes --force-yes install libfreetype6-dev
-sudo apt-get --yes --force-yes install liblcms2-dev
-sudo apt-get --yes --force-yes install libwebp-dev
-sudo apt-get --yes --force-yes install tcl8.6-dev
-sudo apt-get --yes --force-yes install tk8.6-dev
-sudo apt-get --yes --force-yes install libxml2-dev
-sudo apt-get --yes --force-yes install libxslt-dev
-sudo apt-get --yes --force-yes install python-tk
-sudo apt-get --yes --force-yes install python-scipy
-sudo apt-get --yes --force-yes install python-skimage
-sudo apt-get --yes --force-yes install protobuf-compiler
+#!/usr/bin/env bash
+set -e    # Script stops with exit code 1 if something goes wrong.
 
-sudo pip install numpy
-sudo pip install protobuf
-sudo pip install pillow
-sudo pip install tqdm
-sudo pip install scikit-learn
+sudo apt-get --yes --force-yes update
+sudo apt-get --yes --force-yes install git                  \
+                                       cmake                \
+                                       python-pip           \
+                                       python-dev           \
+                                       libboost-all-dev     \
+                                       libtiff5-dev         \
+                                       libjpeg8-dev         \
+                                       zlib1g-dev           \
+                                       libfreetype6-dev     \
+                                       liblcms2-dev         \
+                                       libwebp-dev          \
+                                       tcl8.6-dev           \
+                                       tk8.6-dev            \
+                                       libxml2-dev          \
+                                       libxslt-dev          \
+                                       python-tk            \
+                                       python-scipy         \
+                                       python-skimage       \
+                                       protobuf-compiler
+
+sudo pip install numpy        \
+                 protobuf     \
+                 pillow       \
+                 tqdm         \
+                 scikit-learn
